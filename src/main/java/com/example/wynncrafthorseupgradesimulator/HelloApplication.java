e115261a7d8dae1cb3ca67bd06e590a8c4134120
alonzojp <alonzojp@gmail.com>
2023-01-01 03:01:53 -0800

    Created the algorithm for Tier 2 calculation

--- src/main/java/com/example/wynncrafthorseupgradesimulator/HelloApplication.java
+++ src/main/java/com/example/wynncrafthorseupgradesimulator/HelloApplication.java
@@ -8,6 +8,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    static int purchasedHorses = 0;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -18,6 +21,27 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        // 20% Higher
+        // 50% Same
+        // 30% Lower
+        // Cost: 24 EB
+        // Tiers: Brown, Black, Chestnut, White
+
+//        launch();
+        getBlackHorse();
+        System.out.println("Horses Purchased: " + purchasedHorses);
     }
+
+    public static void getBlackHorse() {
+        int currentTier = 1;
+        purchasedHorses++; // Initial purchase
+        
+        while(!(currentTier == 2)) {
+            purchasedHorses++;
+            if(Math.random() > .8) {
+                currentTier++;
+            }
+        }
+    } // end getBlackHorse()
+
 }
